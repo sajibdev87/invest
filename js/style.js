524c6b9efc6c1fc8311dae0f93125ebaf284de64
sajibdev87 <145119503+sajibdev87@users.noreply.github.com>
2023-10-01 13:07:52 +0600

    y

---
+++ js/style.js
@@ -0,0 +1,17 @@
+
+
+
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        document.getElementById("navbar").style.padding = "0px 10px";
+        document.getElementById("navbar").style.background = "#000";
+        document.getElementById("logo").style.fontSize = "25px";
+    } else {
+        document.getElementById("navbar").style.padding = "0px 10px";
+        document.getElementById("navbar").style.background = "transparent";
+        document.getElementById("logo").style.fontSize = "35px";
+    }
+}
